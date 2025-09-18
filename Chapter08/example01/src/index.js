b789e09bfd9df88757ee8b8bcfd2e8d110993009
ibiscum <ulf.luecking@gmail.com>
2025-09-18 09:41:12 +0200

    ch08 ex01

--- Chapter08/example01/src/index.js
+++ Chapter08/example01/src/index.js
@@ -1,5 +1,10 @@
 import { readFile } from "fs";
 import { resolve } from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export function getLibName() {
   const packagePath = resolve(__dirname, "../package.json");
